b8e1223851f79911ce3a368bd2ac1bec68d338ef
Andy Anderson <andy@clubanderson.com>
2014-10-04 08:59:43 -0400

    first commit

--- tests/init.pp
+++ tests/init.pp
@@ -3,7 +3,7 @@ node default {
   notify { 'enduser-before': }
   notify { 'enduser-after': }
 
-  class { 'ntp':
+  class { 'notifyme':
     require => Notify['enduser-before'],
     before  => Notify['enduser-after'],
   }
